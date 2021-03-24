aa3dbb303d5c3363b81f8a5ef81ef76cf915e7c7
niklashaw16 <76562871+niklashaw16@users.noreply.github.com>
2021-03-24 14:34:00 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,18 @@
+This file is part of Stereoptic Fusion Test 2.
+
+Stereoptic Fusion Test 2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Stereoptic Fusion Test 2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Stereoptic Fusion Test 2.  If not, see <http://www.gnu.org/licenses/>.
+
 var score = 0;
 var scoresaver = [];
 
